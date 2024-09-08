3c2b052f537d63e80e1c0c1b3ac5e663c6f47ff4
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-09-08 07:55:12 +0200

    Añado la barra inferior

--- editor.py
+++ editor.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog # Para el manejo de archivos
 
-
-
-
-
 # Interfaz de usuario
 ## Ventana principal
 
@@ -26,11 +22,20 @@ help = tk.Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Ayuda", menu=help)
 help.add_command(label = "Acerca de...")
 
-#Ventana de texto
+## Ventana de texto
 
 texto = tk.Text(root)
 texto.pack(fill="both", expand=1)
 texto.config(padx=6, pady=4, bd=0, font=("Consolas", 12))
+
+## Mensaje inferior
+lower_text = tk.StringVar()
+lower_text.set("Bienvenido al editor de texto")
+lower_info = tk.Label(root, textvariable=lower_text, justify="right")
+lower_info.pack(side="left")
+
+# Funciones
+
 
 
 # Bucle Principal
